9a534e8c383595f64e95821c5a2698709236e10b
Iann <iconian4@gmail.com>
2025-04-15 00:03:55 +0300

    initialize commit

---
+++ src/components/Article.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+  const renderMinutesToRead = () => {
+    if (minutes < 30) {
+      const coffeeCups = Math.ceil(minutes / 5);
+      return `${'☕️'.repeat(coffeeCups)} ${minutes} min read`;
+    } else {
+      const bentoBoxes = Math.ceil(minutes / 10);
+      return `${'🍱'.repeat(bentoBoxes)} ${minutes} min read`;
+    }
+  };
+
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>{date} · {renderMinutesToRead()}</small>
+      <p>{preview}</p>
+    </article>
+  );
+}
+
+export default Article;
